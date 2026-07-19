3b144b8b8776a19bdf9f2397c402148eda638f24
ThatYiGuy <thelordofnerds@gmail.com>
2026-07-19 19:51:02 -0400

    Fix Windows launcher path quoting

--- launch.cmd
+++ launch.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-set "APP_DIR=%~dp0"
-set "ELECTRON_EXE=%APP_DIR%node_modules\electron\dist\electron.exe"
+for %%I in ("%~dp0.") do set "APP_DIR=%%~fI"
+set "ELECTRON_EXE=%APP_DIR%\node_modules\electron\dist\electron.exe"
 
 if not exist "%ELECTRON_EXE%" (
   echo Creature Companion dependencies are not installed.
@@ -10,4 +10,4 @@ if not exist "%ELECTRON_EXE%" (
   exit /b 1
 )
 
-start "Creature Companion" "%ELECTRON_EXE%" "%APP_DIR%"
+start "Creature Companion" /D "%APP_DIR%" "%ELECTRON_EXE%" "%APP_DIR%"
